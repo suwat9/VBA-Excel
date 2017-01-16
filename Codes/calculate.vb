e17fa805bbb3ac9b9457369ad49d2194bddffff2
Suwat <suwat.tac@mail.pbru.ac.th>
2017-01-16 11:56:18 +0700

    Update calculate.vb

--- Codes/calculate.vb
+++ Codes/calculate.vb
@@ -6,6 +6,7 @@ data.Value = 10
 Range("A6").Select
 Selection.Formula = "=sum(a1:a5)"
 'Move Next Right Cell
+'ค่า Offset (R, C) โดยที่ R ใช้บอกแถวที่จะขยับไป และ C ใช้บอกสดมภ์ที่จะขยับไป จำนวนบวกใช้ขยับค่าให้มากขึ้น จำนวนลบใช้ขยับค่าให้น้อยลง
 Selection.Offset(0, 1).Select
 Selection.Formula = "=average(a1:a5)"
 'Copy & Paste formula
